632665792d7ba7b4b2d7d3516142693e619704cd
balbarka <balbarka>
2024-06-06 12:57:55 +0000

    Update to processed_article_content and transform

--- databricks/_resources/CREATE_TABLE_processed_articles_content.sql
+++ databricks/_resources/CREATE_TABLE_processed_articles_content.sql
@@ -1,3 +1,10 @@
 CREATE TABLE IF NOT EXISTS {uc_name} (
-  a INT)
-USING DELTA
+    id        STRING,
+    pmid      STRING,
+    journal   STRING,
+    title     STRING,
+    year      STRING,
+    citation  STRING,
+    content   STRING)
+-- BRAD TODO: add table properties for managed sync
+USING DELTA;
